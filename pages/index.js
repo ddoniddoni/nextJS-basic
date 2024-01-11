242ad029cc9e11bd3c2e00e18febb1a85edda0de
unknown <psdkei@naver.com>
2024-01-12 02:19:47 +0900

    Update SSR, Page Router

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,17 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const { results } = await (
-                await fetch("/api/movies")).json();
-            setMovies(results);
-        })();
-    }, []);
-
-    return (
-        <div className="container">
-            <Seo title="Home" />
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map(movie => (
-                <div className="movie" key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                    <h4>{movie.original_title}</h4>
-                </div>
-            ))}
-            <style jsx>{`
+export default function Home({ results }) {
+  return (
+    <div className="container">
+      <Seo title="Home" />
+      {results?.map(result => (
+        <div className="movie" key={result.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${result.poster_path}`} />
+          <h4>{result.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
         .container {
           display: grid;
           grid-template-columns: 1fr 1fr;
@@ -43,6 +32,15 @@ export default function Home() {
           text-align: center;
         }
       `}</style>
-        </div>
-    )
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    }
+  }
 }
